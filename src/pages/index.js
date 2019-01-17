7070b8a19f20fa12daebcd669b062ca3671f72fb
Lucas Miller <38452335+lucashowardmiller@users.noreply.github.com>
2019-01-16 23:17:20 -0600

    Update index.js
    Fixed misspelling on line 13.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Welcome to the NDSU Cyber Security Student Association Homepage</h1>
-    <p>We are a club of many individuals that share a common interest of creting technology, and breaking it.</p>
+    <p>We are a club of many individuals that share a common interest of creating technology, and breaking it.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
