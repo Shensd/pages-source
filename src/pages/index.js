6bea77d6c300c142a48ac27bec4ebbe346a31d4e
Jack Hance <jack_e_hance@yahoo.com>
2019-01-20 16:02:47 -0600

    changed image loading on index to inside page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,39 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, StaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hello</h1>
-    <p>Welcome to the NDSU Cyber Security Student Association Homepage</p>
-    <p>We like to have fun here.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/about/">About</Link>
-  </Layout>
+    <StaticQuery
+        query={graphql`
+            query {
+                imgSkimask: file(relativePath: { eq: "csa-skimask.jpg" }) {
+                    childImageSharp {
+                        fluid(maxWidth: 300) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+        `}
+        render={
+            (data) => (
+                <Layout>
+                    <SEO title="Home" />
+                    <h1>Hello</h1>
+                    <p>Welcome to the NDSU Cyber Security Student Association Homepage</p>
+                    <p>We like to have fun here.</p>
+                    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+                        <Img fluid={data.imgSkimask.childImageSharp.fluid} />
+                    </div>
+                    <Link to="/about/">About</Link>
+                </Layout>
+            )
+        }
+    />
 )
 
 export default IndexPage
